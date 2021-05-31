d5fcf8ec8c5b1dbc84644bdac71e580498c429c9
LEETAEHUN <LEETAEHUN@DESKTOP-MNTUSLC>
2021-05-31 13:19:29 +0900

    123

--- sample/src/main/java/com/sample/SampleApplication.java
+++ sample/src/main/java/com/sample/SampleApplication.java
@@ -2,12 +2,18 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootApplication.class);
+	}
 }
